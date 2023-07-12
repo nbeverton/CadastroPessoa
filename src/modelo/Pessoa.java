cc8e2070c63743cbcad76e3eddf013a45687975b
nbeverton <nbeverton@gmail.com>
2023-07-12 16:08:34 -0300

    Initial commit

---
+++ src/modelo/Pessoa.java
@@ -0,0 +1,59 @@
+package modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String endereco;
+    private Double telefone;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String endereco, Double telefone) {
+        super();
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Double getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Double telefone) {
+        this.telefone = telefone;
+    }
+}
